9d8612a61609231e5fefd9d92f8b3a0300839ade
mrartcher <114015891+mrartcher@users.noreply.github.com>
2023-07-25 10:52:18 +0000

    1

---
+++ commands/test/modal.js
@@ -0,0 +1,28 @@
+const { ActionRowBuilder, Events, ModalBuilder, TextInputBuilder, TextInputStyle, SlashCommandBuilder } = require("discord.js");
+import client from "../../index";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("modal")
+        .setDescription("Test modal"),
+    async execute(interaction) {
+        const modal = new ModalBuilder()
+            .setCustomId("testModal")
+            .setTitle("Test modal")
+        const text = new TextInputBuilder()
+            .setCustomId("textTest")
+            .setLabel("Test")
+            .setPlaceholder("Enter text...")
+            .setStyle(TextInputStyle.Short)
+        const textInput = new ActionRowBuilder().addComponents(text);
+        modal.addComponents(firstActionRow);
+        await interaction.showModal(modal);
+
+        client.on(Events.InteractionCreate, async interaction => {
+            if (!interaction.isModalSubmit()) return;
+            if (interaction.customId === 'testModal') {
+                await interaction.reply({ content: 'Your submission was received successfully!' });
+            }
+        });
+    }
+}
