11bfd76fd3b6c7f575c6b47c1e0d7d1b0e889e67
mrartcher <114015891+mrartcher@users.noreply.github.com>
2023-07-25 08:07:08 +0000

    1

--- commands/test/ping.js
+++ commands/test/ping.js
@@ -9,6 +9,6 @@ module.exports = {
         .setColor(0x06D551)
         .setTitle("Pong!")
         .setDescription(`Ping: ${interaction.client.ws.ping}ms`)
-		await interaction.reply('pong');
+		await interaction.reply({embed: [embed]});
 	}
 }
