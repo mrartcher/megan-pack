40a86502f61a540a8b47ec17bc511178739855a4
mrartcher <114015891+mrartcher@users.noreply.github.com>
2023-07-25 09:27:34 +0000

    1

--- commands/vacancy/vacancy.js
+++ commands/vacancy/vacancy.js
@@ -7,17 +7,21 @@ module.exports = {
         .setDescription("Send vacancys")
         .setDMPermission(false)
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
     async execute(interaction) {
         const embed = new EmbedBuilder()
-            .setColor("0x767676")
+            .setColor(0x767676)
             .setTitle("Открытия вакансий")
             .setDescription("Нажмите на кнопку ниже для открытия вакансии")
+
         const helper = new ButtonBuilder()
             .setCustomId("helper-vacancy")
             .setStyle(ButtonStyle.Success)
-            .setLabel("Отурыть набор на пост хелпера")
+            .setLabel("Отурыть вакансии на пост хелпера")
+            
         const row = new ActionRowBuilder()
             .addComponents(helper)
+
         await interaction.reply({embeds: [embed], components: [row], ephemeral: true})
     }
 }
