4d5b2395effc1fc3da0d7cb65d656c0d61dbc647
mrartcher <114015891+mrartcher@users.noreply.github.com>
2023-07-25 08:08:00 +0000

    1

--- commands/test/ping.js
+++ commands/test/ping.js
@@ -6,9 +6,9 @@ module.exports = {
     .setDescription("Replays with Pong!"),
 	async execute(interaction) {
         const embed = new EmbedBuilder()
-        .setColor(0x06D551)
-        .setTitle("Pong!")
-        .setDescription(`Ping: ${interaction.client.ws.ping}ms`)
+            .setColor(0x06D551)
+            .setTitle("Pong!")
+            .setDescription(`Ping: ${interaction.client.ws.ping}ms`)
 		await interaction.reply({embed: [embed]});
 	}
 }
