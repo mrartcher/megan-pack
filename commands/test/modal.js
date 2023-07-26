7e578a0321b1ea9e14f5d63ad445eca8a68d34cc
mrartcher <114015891+mrartcher@users.noreply.github.com>
2023-07-26 07:22:37 +0000

    1

--- commands/test/modal.js
+++ commands/test/modal.js
@@ -1,11 +1,34 @@
-const { SlashCommandBuilder } = require("discord.js");
-import client from "../../index";
+const { SlashCommandBuilder, Events } = require("discord.js");
+const client = require("../../index");
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("modal")
     .setDescription("Test modal"),
     async execute (interaction) {
-        // some code
+        client.on(Events.InteractionCreate, async interaction =>{
+            if (interaction.commandName === "modal") {
+                const modal = new ModalBuilder()
+                    .setCustomId("testModal")
+                    .setTitle("Test modal")
+                const text = new TextInputBuilder()
+                    .setCustomId("textTest")
+                    .setLabel("Test")
+                    .setPlaceholder("Enter text...")
+                    .setStyle(TextInputStyle.Short)
+                const textInput = new ActionRowBuilder().addComponents(text);
+                modal.addComponents(textInput);
+                await interaction.showModal(modal);
+        
+                client.on(Events.InteractionCreate, async interaction =>{
+                    if (!interaction.isModalSubmit()) return;
+                    
+                    if (interaction.customId === "testModal") {
+                        const text = interaction.fields.getTextInputValue('textTest')
+                        await interaction.reply({ content: text })
+                    }
+                });
+            }
+        });
     }
 }
--- index.js
+++ index.js
@@ -34,29 +34,6 @@ client.on(Events.InteractionCreate, async interaction => {
 
 	if (!command) return;
 
-	if (interaction.commandName === "modal") {
-		const modal = new ModalBuilder()
-            .setCustomId("testModal")
-            .setTitle("Test modal")
-        const text = new TextInputBuilder()
-            .setCustomId("textTest")
-            .setLabel("Test")
-            .setPlaceholder("Enter text...")
-            .setStyle(TextInputStyle.Short)
-        const textInput = new ActionRowBuilder().addComponents(text);
-        modal.addComponents(textInput);
-        await interaction.showModal(modal);
-
-        client.on(Events.InteractionCreate, async interaction =>{
-            if (!interaction.isModalSubmit()) return;
-            
-            if (interaction.customId === "testModal") {
-                const text = interaction.fields.getTextInputValue('textTest')
-				await interaction.reply({ content: text })
-            }
-        });
-	}
-
 	try {
 		await command.execute(interaction);
 	} catch (error) {
