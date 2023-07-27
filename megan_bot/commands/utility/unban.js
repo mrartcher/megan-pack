e07c2553a138e4ec53a4bdd36a52455f5bfa71db
mrartcher <114015891+mrartcher@users.noreply.github.com>
2023-07-27 19:31:23 +0000

    1

---
+++ megan_bot/commands/utility/unban.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, EmbedBuilder, embedLength } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ban")
+        .setDescription("Ban member")
+        .addUserOption(op => op
+            .setName("user")
+            .setDescription("User to ban")
+            .setRequired(true)),
+    async execute(interaction) {
+        const user = interaction.options.getUser("user")
+
+        const embedS = new EmbedBuilder()
+            .setTitle("Unbaned")
+            .setDescription(`User: ${user} has been unbaned.`)
+            .setColor(0x611508)
+        
+        const embedL = new EmbedBuilder()
+            .setTitle("User unbaned")
+            .setDescription(`User: ${user}`)
+            .setColor(0x611508)
+        
+        await interaction.reply({embeds: [embedS]})
+
+        const chanel = interaction.guild.channels.cache.get('1134204456175681776');
+        chanel.send({embeds: [embedL]})
+
+        await interaction.guild.members.unban(user)
+    }
+}
