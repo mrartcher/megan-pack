15b58f7fb946ca398e5f8a9efba56118094b84b1
mrartcher <114015891+mrartcher@users.noreply.github.com>
2023-07-27 19:41:36 +0000

    1

--- megan_bot/commands/utility/unban.js
+++ megan_bot/commands/utility/unban.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, embedLength } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
