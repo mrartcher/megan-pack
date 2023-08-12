2f0dd02038d3a82d036e3b88ccf1ec89a1e3cb99
mrartcher <retin117@gmail.com>
2023-08-12 22:11:40 +0300

    1

--- megan_bot/commands/rules/rek.js
+++ megan_bot/commands/rules/rek.js
@@ -6,17 +6,6 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
         .setDMPermission(false),
     async execute(interaction) {
-        const url = 'https://discord.gg/XhNQG4B9'
-        const chanell = interaction.guild.channels.cache.get('1134408862494109728');
 
-        const embed = new EmbedBuilder()
-            .setColor(0x611508)
-            .setTitle("Топовый гриферский сервер")
-            .setDescription("DarkGrief - сервер гриферского выжывания с отзывчивой администрацией")
-            .addFields(
-                {name: "Ссылка", value: url}
-            )
-
-        chanell.send({embeds: [embed]})
     }
 }
