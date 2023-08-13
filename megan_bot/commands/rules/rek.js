9fc7c886d17345ed6415715318dc5272158216eb
mrartcher <retin117@gmail.com>
2023-08-13 15:44:38 +0300

    1

--- megan_bot/commands/rules/rek.js
+++ megan_bot/commands/rules/rek.js
@@ -34,39 +34,27 @@ module.exports = {
             .setMinLength(10)
             .setRequired(true)
 
-        const urlInput = new TextInputBuilder()
-            .setCustomId("urlInput")
-            .setLabel("Enter url")
-            .setPlaceholder("Enter the text...")
-            .setStyle(TextInputStyle.Short)
-            .setMinLength(3)
-            .setRequired(true)
-
         const row = new ActionRowBuilder().addComponents(titleInput)
         const row1 = new ActionRowBuilder().addComponents(bodyInput)
-        const row2 = new ActionRowBuilder().addComponents(urlInput)
 
-        messageModal.addComponents(row, row1, row2)
+        messageModal.addComponents(row, row1)
 
         await interaction.showModal(messageModal)
 
         const filter = (interaction) => interaction.customId === 'msdModal'
 
-        await interaction.awaitModalSubmit({filter, time: 30_000})
+        await interaction.awaitModalSubmit({filter, time: 60_000})
             .then((modalInteraction) => {
                 const title = modalInteraction.fields.getTextInputValue("titleInput")
                 const body = modalInteraction.fields.getTextInputValue("bodyInput")
-                const url = modalInteraction.fields.getTextInputValue("urlInput")
-                const channel = modalInteraction.guild.channels.cache.get('1140187214320521317');
+                const channel = modalInteraction.guild.channels.cache.get('1134408862494109728');
 
                 const embed = new EmbedBuilder()
                     .setColor(0x611508)
                     .setTitle(title)
                     .setDescription(body)
-                    .addFields(
-                        {name: "Ссылка", value: url}
-                    )
 
+                channel.send({content: "@everyone"})
                 channel.send({embeds: [embed]})
 
                 modalInteraction.reply({content: "Modal success submitted", ephemeral: true})
